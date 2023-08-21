4e2e499984958fd3ba3fd93ddf2c3a9c2e42202f
scivision <scivision@users.noreply.github.com>
2023-08-21 10:28:34 -0400

    satisfy valgrind

--- test/mkdtemp/test_mkdtemp.f90
+++ test/mkdtemp/test_mkdtemp.f90
@@ -17,15 +17,17 @@ character(2048, kind=C_CHAR) :: path
 character(:), allocatable :: temp_dir
 !! arbitrary buffer size
 
-integer(C_INT) :: i
 integer(C_SIZE_T) :: L
 
 L = mkdtemp_f(path, len(path, kind=C_SIZE_T))
 if(L == 0) error stop "ERROR: Fortran mkdtemp got empty string"
 
 allocate(character(L) :: temp_dir)
-temp_dir = path(1:L)
+temp_dir = path(:L)
 
-print *, "OK: Fortran mkdtemp: " // temp_dir
+print '(a)', "OK: Fortran mkdtemp: " // temp_dir
+
+deallocate(temp_dir)
+!! for valgrind, not always detected/needed in Fortran main programs.
 
 end program
