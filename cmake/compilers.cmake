0f6571da67209d157dbadddff0516ee0de33e919
scivision <scivision@users.noreply.github.com>
2023-08-20 21:53:49 -0400

    optimize compile options

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,8 +1,20 @@
+include(CheckIncludeFile)
+include(CheckSymbolExists)
+
 if((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.1.0") OR
     CMAKE_CXX_COMPILER_ID STREQUAL "NVHPC")
     link_libraries(stdc++fs stdc++)
     set(CMAKE_REQUIRED_LIBRARIES stdc++fs stdc++)
 endif()
+
+if(CMAKE_C_COMPILER_ID STREQUAL "IntelLLVM")
+  add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Rno-debug-disables-optimization>)
+  add_link_options($<$<COMPILE_LANGUAGE:C,CXX>:-Rno-debug-disables-optimization>)
+endif()
+
+add_compile_definitions(
+  "$<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS;_CRT_NONSTDC_NO_WARNINGS>"
+)
 
 # fixes errors about needing -fPIE
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
--- options.cmake
+++ options.cmake
@@ -2,12 +2,3 @@ option(cppcheck "Run cppcheck on the code")
 
 # Necessary for shared library with Visual Studio / Windows oneAPI -- creates .lib file to acommpany .dll -- must be BEFORE targets are declared.
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
-
-# Rpath options necessary for shared library install to work correctly in user projects
-set(CMAKE_INSTALL_NAME_DIR ${CMAKE_INSTALL_FULL_LIBDIR})
-set(CMAKE_INSTALL_RPATH ${CMAKE_INSTALL_FULL_LIBDIR})
-set(CMAKE_INSTALL_RPATH_USE_LINK_PATH true)
-
-if(NOT PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
-  file(GENERATE OUTPUT .gitignore CONTENT "*")
-endif()
