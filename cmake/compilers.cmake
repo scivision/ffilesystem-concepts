61cd3e216908aea56355ef862baf78567f83bc19
scivision <scivision@users.noreply.github.com>
2023-08-26 23:19:53 -0400

    no backtrace on release

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -7,6 +7,10 @@ if((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_
     set(CMAKE_REQUIRED_LIBRARIES stdc++fs stdc++)
 endif()
 
+if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+  add_compile_options($<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>)
+endif()
+
 if(CMAKE_C_COMPILER_ID STREQUAL "IntelLLVM")
   add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Rno-debug-disables-optimization>)
   add_link_options($<$<COMPILE_LANGUAGE:C,CXX>:-Rno-debug-disables-optimization>)
@@ -15,13 +19,6 @@ endif()
 add_compile_definitions(
   "$<$<AND:$<COMPILE_LANGUAGE:C,CXX>,$<BOOL:${MSVC}>>:_CRT_SECURE_NO_WARNINGS;_CRT_NONSTDC_NO_WARNINGS>"
 )
-
-check_include_file("unistd.h" HAVE_UNISTD_H)
-set(_mkd_inc "stdlib.h")
-if(HAVE_UNISTD_H)
-  list(APPEND _mkd_inc "unistd.h")
-endif()
-check_symbol_exists(mkdtemp "${_mkd_inc}" HAVE_MKDTEMP)
 
 # fixes errors about needing -fPIE
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
