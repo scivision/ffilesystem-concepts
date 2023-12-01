b259add2735226a3502482705bdefe7c6fe20c1d
Michael Hirsch <scivision@users.noreply.github.com>
2023-11-30 20:32:51 -0500

    consolidate CMakeLists.txt

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,11 +1,12 @@
 include(CheckIncludeFile)
 include(CheckSymbolExists)
 
-if((CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.1.0") OR
-    CMAKE_CXX_COMPILER_ID STREQUAL "NVHPC")
+if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.1.0")
     link_libraries(stdc++fs stdc++)
     set(CMAKE_REQUIRED_LIBRARIES stdc++fs stdc++)
 endif()
+# GCC < 9.1 needs -lstdc++ to avoid C main program link error
+# NVHPC at least 23.11 and newer doesn't need the flags.
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   add_compile_options($<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>)
