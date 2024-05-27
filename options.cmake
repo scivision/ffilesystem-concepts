0f4ec1f601ecfd79a6943e90819075334f71b096
scivision <scivision@users.noreply.github.com>
2024-05-27 16:46:50 -0400

    correct fortran optino

--- options.cmake
+++ options.cmake
@@ -1,4 +1,5 @@
 option(cppcheck "Run cppcheck on the code")
+option(fortran "use Fortran" on)
 
 # Necessary for shared library with Visual Studio / Windows oneAPI -- creates .lib file to acommpany .dll -- must be BEFORE targets are declared.
 set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS true)
