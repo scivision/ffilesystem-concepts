0fd03804c3ece34d2c0453e53590ff7beb26bb32
scivision <scivision@users.noreply.github.com>
2024-12-12 15:03:56 -0500

    <filesystem> API check

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,12 +1,51 @@
 include(CheckIncludeFile)
 include(CheckSymbolExists)
 
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.1.0")
-    link_libraries(stdc++fs stdc++)
-    set(CMAKE_REQUIRED_LIBRARIES stdc++fs stdc++)
+if(CMAKE_VERSION VERSION_LESS 3.25 AND CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  set(LINUX true)
 endif()
-# GCC < 9.1 needs -lstdc++ to avoid C main program link error
-# NVHPC at least 23.11 and newer doesn't need the flags.
+
+if(NOT MSVC AND NOT DEFINED ffilesystem_stdcpp_version)
+
+message(CHECK_START "Checking C++ standard library version")
+# Intel, IntelLLVM and NVHPC on Linux use GNU libstdc++, so we need to extract the libstdc++ version
+try_run(ffilesystem_stdcpp_run ffilesystem_stdcpp_build_ok
+  ${CMAKE_CURRENT_BINARY_DIR}/libstdcpp_version
+  SOURCES ${CMAKE_CURRENT_LIST_DIR}/libstdcpp_version.cpp
+  RUN_OUTPUT_VARIABLE _stdcpp_version
+)
+
+if(NOT ffilesystem_stdcpp_run EQUAL 0)
+  message(CHECK_FAIL "Could not determine GNU libstdc++ version ${ffilesystem_stdcpp_build_ok} ${ffilesystem_stdcpp_run} ${_stdcpp_version}")
+else()
+  string(STRIP "${_stdcpp_version}" _stdcpp_version)
+  set(ffilesystem_stdcpp_version "${_stdcpp_version}" CACHE STRING "C++ standard library version")
+  message(CHECK_PASS "${ffilesystem_stdcpp_version}")
+endif()
+
+endif()
+
+if(ffilesystem_stdcpp_run EQUAL 0)
+  if(ffilesystem_stdcpp_version MATCHES "GNU ([0-9]+)")
+    if(CMAKE_MATCH_1 LESS 9)
+      set(GNU_stdfs stdc++fs stdc++)
+    else()
+      message(VERBOSE "GNU libstdc++ ${ffilesystem_stdcpp_version} is new enough to not need -lstdc++")
+    endif()
+  else()
+    message(VERBOSE "Did not determine GNU libstdc++ version ${ffilesystem_stdcpp_version}")
+  endif()
+endif()
+
+
+if(GNU_stdfs)
+  set(CMAKE_REQUIRED_LIBRARIES ${GNU_stdfs})
+  message(STATUS "adding C++ library flags ${GNU_stdfs}")
+  link_libraries(${GNU_stdfs})
+endif()
+
+
+add_compile_options("$<$<COMPILE_LANG_AND_ID:CXX,AppleClang,Clang,GNU>:-Wold-style-cast>")
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   add_compile_options($<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Release>>:-fno-backtrace>)
