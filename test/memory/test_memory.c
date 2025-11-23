153b5025c4c966ee3765492f786924e22d2fc5a2
scivision <scivision@users.noreply.github.com>
2025-11-23 12:39:44 -0500

    add free memory

--- test/memory/test_memory.c
+++ test/memory/test_memory.c
@@ -1,9 +1,22 @@
+// functions:
+//  fs_total_sys_memory() - returns total system memory in bytes
+//  GetFreeMemory()       - returns free system memory in bytes
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#else
+#elif defined(__APPLE__)
+#include <mach/mach.h>
+#include <sys/sysctl.h>
+#elif defined(__linux__)
+#include <sys/sysinfo.h>
+#endif
+
+#if !defined(_WIN32)
 #include <unistd.h>
 #endif
+
+#include <limits.h>
 
 #include <errno.h>
 
@@ -38,6 +51,62 @@ unsigned long long fs_total_sys_memory()
 #endif
 }
 
+
+long fs_get_free_memory() {
+  // https://github.com/ninja-build/ninja/pull/2605
+#if defined(_WIN32) || defined(__CYGWIN__)
+  static long committed_idle = LONG_MAX;
+  MEMORYSTATUSEX status;
+  status.dwLength = sizeof(status);
+  GlobalMemoryStatusEx(&status);
+  const long committed = (status.ullTotalPageFile - status.ullAvailPageFile);
+  // since system use committed memory normaly, we store the smallest amount we have seen to guess how much
+  // paging is non-ninja related
+  committed_idle = (committed_idle < committed) ? committed_idle : committed;
+
+  return status.ullAvailPhys - (committed - committed_idle);
+#elif defined(__APPLE__)
+  static uint64_t swapped_idle = LONG_MAX;
+  vm_size_t page_size;
+  vm_statistics64_data_t vm_stats;
+
+  mach_port_t port = mach_host_self();
+  mach_msg_type_number_t count = sizeof(vm_stats) / sizeof(natural_t);
+
+  size_t swap_stats_size = sizeof(struct xsw_usage);
+  struct xsw_usage swap_stats;
+
+  int ctl[] = {CTL_VM, VM_SWAPUSAGE};
+
+  int result = host_page_size(port, &page_size);
+  result += host_statistics64(port, HOST_VM_INFO,
+    (host_info64_t)(&vm_stats), &count);
+  result -= sysctl(ctl, 2, &swap_stats, &swap_stats_size, NULL, 0);
+
+  if (KERN_SUCCESS != result)
+    // information not available
+    return LONG_MAX;
+
+  // inactive is memory is marked to be moved to swap or is fs cache and should be considered as free
+  int64_t free_memory = (vm_stats.free_count + vm_stats.inactive_count ) * page_size;
+  // since inactive memory can be moved to the swap this value will be inexact, and i don't belive that there is a clean solution for that.
+  uint64_t used_swap = swap_stats.xsu_used;
+  swapped_idle = (used_swap < swapped_idle) ? used_swap : swapped_idle;
+  return free_memory - (used_swap - swapped_idle);
+#elif defined(__linux__)
+  static long swapped_idle = LONG_MAX;
+  struct sysinfo infos;
+  sysinfo(&infos);
+  const long swapped = (infos.totalswap - infos.freeswap);
+  // since system use committed memory normally, store the smallest amount we have seen
+  swapped_idle = (swapped_idle < swapped) ? swapped_idle : swapped;
+  return infos.freeram - (swapped - swapped_idle);
+#else
+  return LONG_MAX;
+#endif
+}
+
+
 int main(void)
 {
   unsigned long long t = fs_total_sys_memory();
@@ -45,5 +114,12 @@ int main(void)
     return EXIT_FAILURE;
 
   printf("Total system memory: %llu bytes\n", t);
+
+  long f = fs_get_free_memory();
+  if(f <= 0)
+    return EXIT_FAILURE;
+
+  printf("Free system memory:  %ld bytes\n", f);
+
   return EXIT_SUCCESS;
 }
